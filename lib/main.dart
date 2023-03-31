240543d33716fdeb94ab728b0b2f98ced197e963
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-03-31 11:37:32 +0900

    nestedScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 
 import 'package:flutter/material.dart';
-import 'package:sliver_app/ad.dart';
 import 'package:sliver_app/diary.dart';
+
+import 'ad.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,55 +25,114 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        // scrollDirection: Axis.horizontal, //방향 제어가능
-        // 전체는 세로지만, 가로 슬라이딩을 하기위해서는 nestedScrollView 이용
-        slivers: [
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
           SliverAppBar(
-            snap: true,
+            snap: true, // 튕기듯이 나오는 효과.
             floating: true,
             title: Text("SliverAppbar"),
             pinned: false,
-            //고정
             expandedHeight: 250,
-            //최대크기 늘려줌 //기본 60
             flexibleSpace: Container(
               child: Center(
-                child: Text(
-                  "FlexibleSpace",
-                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-                ),
+                child: Text("FlexibleSpace", style: TextStyle(fontSize: 50)),
               ),
             ),
           ),
-          SliverAppBar(
-            title: Text("SliverAppbar"),
-            pinned: true, //고정
-          ),
-          // SliverList(delegate: SliverChildListDelegate), //아이템이 정해져 있고 계산을 하지 않을 경우 사용
-          // Sliver를 쓰면 슬리버 앱바가 들어가고 리스트가 들어갈수도 있는데, 고정된 크기의 박스를 넣고싶으면 SliverToBoxAdapter 사용
-          SliverToBoxAdapter(
-            child: Container(
-              color: Colors.redAccent,
-              height: 200,
-            ),
-          ),
-
-          SliverFixedExtentList(
-              itemExtent: 100, // 아이템들의 높이를 쌩까고 얘가 정함
-              delegate: SliverChildBuilderDelegate(
-                //계산이 필요하면 무조건 Builder 사용해야함
-                childCount: 50,
-                (context, index) {
-                  if (index % 5 == 0 && index != 0) {
-                    return Ad((index / 5).toInt());
-                  } else
-                    return Diary(index);
-                },
-              ))
         ],
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text("SliverAppbar"),
+              pinned: true,
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                height: 200.0,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 200.0,
+                      child: Container(
+                        child: Text('data'),
+                        alignment: Alignment.center,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            SliverFixedExtentList(
+              itemExtent: 100,
+              delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                  if (index % 4 == 0 && index != 0) {
+                    return Ad((index / 4).toInt());
+                  } else {
+                    return Diary(index);
+                  }
+                },
+                childCount: 50,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Container(
+                color: Colors.red,
+                height: 200,
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+
+  CustomScrollView buildCustomScrollView() {
+    return CustomScrollView(
+      // scrollDirection: Axis.horizontal, //방향 제어가능
+      // 전체는 세로지만, 가로 슬라이딩을 하기위해서는 nestedScrollView 이용
+      slivers: [
+        SliverAppBar(
+          snap: true,
+          floating: true,
+          title: Text("SliverAppbar"),
+          pinned: false,
+          //고정
+          expandedHeight: 250,
+          //최대크기 늘려줌 //기본 60
+          flexibleSpace: Container(
+            child: Center(
+              child: Text(
+                "FlexibleSpace",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+        SliverAppBar(
+          title: Text("SliverAppbar"),
+          pinned: true, //고정
+        ),
+      ],
+    );
+  }
+
+  SliverFixedExtentList _buildSliverFixedExtentList() {
+    return SliverFixedExtentList(
+            itemExtent: 100, // 아이템들의 높이를 쌩까고 얘가 정함
+            delegate: SliverChildBuilderDelegate(
+              //계산이 필요하면 무조건 Builder 사용해야함
+              childCount: 50,
+              (context, index) {
+                if (index % 5 == 0 && index != 0) {
+                  return Ad((index / 5).toInt());
+                } else
+                  return Diary(index);
+              },
+            ));
   }
 
   SliverList buildSliverList() {
